e76f19d037581444ada94dbcf4cda3ca46398f2e
Adriano <adriano_alves_2008@hotmail.com>
2022-10-07 10:42:45 -0300

    css navbar e footer

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom'
+
+import styles from './Navbar.module.css'
+
+function Navbar() {
+  return (
+    <nav className={styles.navbar}>
+      <NavLink to="/" className={styles.brand}>
+        Mini <span>Blog</span>
+      </NavLink>
+      <ul className={styles.links_list}>
+        <li>
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? styles.active : '')}
+          >
+            Sobre
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
