e8aacf0865a7a2cf6e20de56f4a77d1dc43f6afc
Adriano <adriano_alves_2008@hotmail.com>
2022-10-20 20:29:43 -0300

    css do postdetail

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,13 +1,13 @@
-// import styles from 'PostDetail.module.css'
+import styles from './PostDetail.module.css'
 import { Link } from 'react-router-dom'
 
 function PostDetail({ post }) {
   return (
-    <div>
+    <div className={styles.post_detail}>
       <img src={post.image} alt={post.title} />
       <h2>{post.title}</h2>
-      <p>{post.createdBy}</p>
-      <div>
+      <p className={styles.createdby}>{post.createdBy}</p>
+      <div className={styles.tags}>
         {post.tagsArray.map(tag => (
           <p key={tag}>
             <span>#</span>
