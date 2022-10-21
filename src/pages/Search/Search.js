188481c13ed55f8dc8291bc9d8e636b86674ed6e
Adriano <adriano_alves_2008@hotmail.com>
2022-10-21 10:27:40 -0300

    exibindo resultados da busca

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+
+import styles from './Search.module.css'
+
+//components
+import PostDetail from '../../components/PostDetail'
 
 //hooks
 import { useFetchDocuments } from '../../hooks/useFetchDocuments'
@@ -7,10 +12,22 @@ import { useQuery } from '../../hooks/useQuery'
 function Search() {
   const query = useQuery()
   const search = query.get('q')
+
+  const { documents: posts } = useFetchDocuments('posts', search)
   return (
-    <div>
+    <div className={styles.search_container}>
       <h2>Search</h2>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length == 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts a partir da sua busca...</p>
+            <Link to="/" className="btn btn-dark">
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts && posts.map(post => <PostDetail key={post.id} post={post} />)}
+      </div>
     </div>
   )
 }
